b1b16c027618be3eda345fb12ba852ae97c0a759
Delwyn de Villiers <delwyn.d@gmail.com>
2013-10-22 16:45:57 +0100

    add input element class to wrapper

--- packages/ember-easyForm/lib/views/base_view.js
+++ packages/ember-easyForm/lib/views/base_view.js
@@ -1,14 +1,1 @@
-Ember.EasyForm.BaseView = Ember.View.extend({
-  getWrapperConfig: function(configName) {
-    var wrapper = Ember.EasyForm.Config.getWrapper(this.get('wrapper'));
-    return wrapper[configName];
-  },
-  wrapper: Ember.computed(function() {
-    var wrapperView = this.nearestWithProperty('wrapper');
-    if (wrapperView) {
-      return wrapperView.get('wrapper');
-    } else {
-      return 'default';
-    }
-  })
-});
+Ember.EasyForm.BaseView = Ember.View.extend(Ember.EasyForm.WrapperConfigMixin);
